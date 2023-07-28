7ae2af147c4c03b9ce9a1b3012f5fee68a09230d
xdianovx <dianov.js@gmail.com>
2023-07-28 20:39:34 +1000

    222

--- app/Http/Controllers/API/V1/Clients/ProfileController.php
+++ app/Http/Controllers/API/V1/Clients/ProfileController.php
@@ -26,11 +26,12 @@ class ProfileController extends Controller
 
        public function userProfile()
        {
-        $client = Client::where('id', auth()->user('api')->id);
-        if($client->value('avatar_image')):
-            $avatar = env('API_URL') . Storage::url('api/v1',$client->value('avatar_image'));
-        elase:
+        $avatar = null;
+        $client = Client::find(auth()->user('api')->id);
+        if(!empty($client->avatar_image)):
             $avatar = null;
+        elase:
+            $avatar = env('API_URL') . Storage::url($client->avatar_image);
         endif;
         return response()->json([
             'id' => $client->value('id'),
@@ -146,18 +147,14 @@ class ProfileController extends Controller
        // Сохраняем файл
        $path = $request->file('avatar_image')->storeAs('public', $fileNameToStore);
      }
-     $client = Client::where('id', auth()->user('api')->id);
-     if($client->value('avatar_image')):
-         $avatar = env('API_URL') . Storage::url('api/v1',$client->value('avatar_image'));
-     elase:
-         $avatar = null;
-     endif;
+   //   // При выводе файла на странице нудно будет прибавить в начале "storage/"
+   //   $fileNameToStore . "storage/";
            $client = Client::where('id', auth()->user()->id);
            $client_id = $client->value('id');
            Client::where('id', $client_id)->update(['avatar_image'=> $path]);
            return response()->json([
                'message' => 'success',
-               'avatar_image' => $avatar,
+               'avatar_image' => env('API_URL') . Storage::url($client->value('avatar_image')),
            ], 200);
        }
    
