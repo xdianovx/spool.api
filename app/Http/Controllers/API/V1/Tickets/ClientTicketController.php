ee1312b7e28ed600b93a3747593169c0c189077c
Your Name <you@example.com>
2023-10-18 12:42:13 +0300

    asd

--- app/Http/Controllers/API/V1/Tickets/ClientTicketController.php
+++ app/Http/Controllers/API/V1/Tickets/ClientTicketController.php
@@ -52,7 +52,7 @@ class ClientTicketController extends Controller
             "RebillId" => $req->rebill_id,
             'Amount' => $req->amount,
             'Currency' => $req->currency,
-            'Description' => "dafdsfdsf" . $req->user_id,
+            'Description' => "user_id=" . $req->user_id . ";" . "ticket_id=" .  $req->ticket_id,
             "ExtraData" => [
                 "user_id" => $req->user_id,
                 "ticket_id" => $req->ticket_id
