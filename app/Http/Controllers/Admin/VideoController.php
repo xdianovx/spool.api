73e6c3db06b8dcaea8b9d741d31865a56a02f418
xdianovx <dianov.js@gmail.com>
2023-11-04 17:26:05 +1000

    111

--- app/Http/Controllers/Admin/VideoController.php
+++ app/Http/Controllers/Admin/VideoController.php
@@ -28,42 +28,42 @@ class VideoController extends Controller
     }
 
     public function create()
-    { 
+    {
         // $url = 'https://cdn.spoolapp.ru/secret/';
         // $client = Http::get($url)->body();
         // $res = json_decode($client);
         // return response()->json($res);
         $partner_companies = Partners_company::all();
         $categories = Category::all();
-        return view('videos.create',compact('partner_companies','categories'));
+        return view('videos.create', compact('partner_companies', 'categories'));
     }
 
     public function show(Video $video)
     {
         return view('videos.show', compact('video'));
     }
-    
+
     public function edit(Video $video)
-    { 
+    {
         // $url = 'https://cdn.spoolapp.ru/secret/';
         // $client = Http::get($url)->body();
         // $res = json_decode($client);
         $user_tags = $video->tags()->where('user_id', Auth::user()->id)->paginate(1000);
         $partner_companies = Partners_company::all();
         $categories = Category::all();
-        return view('videos.edit', compact('video','partner_companies','categories','user_tags'));
+        return view('videos.edit', compact('video', 'partner_companies', 'categories', 'user_tags'));
     }
 
     public function store(VideoStoreRequest $request)
     {
-      
+
         $data = $request->validated();
         if ($request->hasFile('image')) {
             // Имя и расширение файла
             $filenameWithExt = $request->file('image')->getClientOriginalName();
             // Только оригинальное имя файла
             $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            $filename = str_replace(' ','_',$filename);
+            $filename = str_replace(' ', '_', $filename);
             // Расширение
             $extention = $request->file('image')->getClientOriginalExtension();
             // Путь для сохранения
@@ -76,7 +76,7 @@ class VideoController extends Controller
             $filenameWithExt = $request->file('image_banner')->getClientOriginalName();
             // Только оригинальное имя файла
             $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            $filename = str_replace(' ','_',$filename);
+            $filename = str_replace(' ', '_', $filename);
             // Расширение
             $extention = $request->file('image_banner')->getClientOriginalExtension();
             // Путь для сохранения
@@ -85,25 +85,27 @@ class VideoController extends Controller
             $data['image_banner'] = $request->file('image_banner')->storeAs('public', $fileNameToStore);
         }
 
-       $video = Video::firstOrCreate($data);
-       if($video->category->video_availability == false):
+        $video = Video::firstOrCreate($data);
+        if ($video->category->video_availability == false) :
             $video->category->update([
-               'video_availability'=> true
-           ]);
-       endif;
-        return redirect()->route('video.edit',$video->id)->with('status', 'video-created');
+                'video_availability' => true
+            ]);
+        endif;
+        return redirect()->route('video.edit', $video->id)->with('status', 'video-created');
     }
 
     public function update(VideoUpdateRequest $request, $video_id)
     {
+
         $video = Video::whereId($video_id)->firstOrFail();
+        $category = Category::where('id', $video->category_id)->first();
         $data = $request->validated();
         if ($request->hasFile('image')) {
             // Имя и расширение файла
             $filenameWithExt = $request->file('image')->getClientOriginalName();
             // Только оригинальное имя файла
             $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            $filename = str_replace(' ','_',$filename);
+            $filename = str_replace(' ', '_', $filename);
             // Расширение
             $extention = $request->file('image')->getClientOriginalExtension();
             // Путь для сохранения
@@ -116,7 +118,7 @@ class VideoController extends Controller
             $filenameWithExt = $request->file('image_banner')->getClientOriginalName();
             // Только оригинальное имя файла
             $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            $filename = str_replace(' ','_',$filename);
+            $filename = str_replace(' ', '_', $filename);
             // Расширение
             $extention = $request->file('image_banner')->getClientOriginalExtension();
             // Путь для сохранения
@@ -124,53 +126,136 @@ class VideoController extends Controller
             // Сохраняем файл
             $data['image_banner'] = $request->file('image_banner')->storeAs('public', $fileNameToStore);
         }
+
         $video->update($data);
 
-        if($video->category->video_availability == false):
-            $video->category->update([
-                'video_availability'=> true
-            ]);
+        if (is_null($category)) :
+        else :
+            if ($category->children()->exists()) :
+                $videos_count = $category->videos->count();
+                foreach ($category->children as $cild) :
+                    $videos_count = $videos_count + $cild->videos->count();
+                endforeach;
+                if ($videos_count == 0) :
+                    $category->update([
+                        'video_availability' => false
+                    ]);
+                else :
+                    $category->update([
+                        'video_availability' => true
+                    ]);
+                endif;
+            elseif ($category->parent()->exists()) :
+
+                $videos_count = $category->parent->videos->count();
+                foreach ($category->parent->children as $cild) :
+                    $videos_count = $videos_count + $cild->videos->count();
+                endforeach;
+
+                if ($videos_count == 0) :
+                    $category->parent->update([
+                        'video_availability' => false
+                    ]);
+                else:
+                    $category->parent->update([
+                        'video_availability' => true
+                    ]);
+                endif;
+            endif;
+            if ($category->parent()->exists()) :
+                if ($category->videos->count() == 0) :
+                    $category->update([
+                        'video_availability' => false
+                    ]);
+                else :
+                    $category->update([
+                        'video_availability' => true
+                    ]);
+                endif;
+            endif;
         endif;
+
+
+        if ($video->category->children()->exists()) :
+            $videos_count = $video->category->videos->count();
+            foreach ($video->category->children as $cild) :
+                $videos_count = $videos_count + $cild->videos->count();
+            endforeach;
+            if ($videos_count == 0) :
+                $video->category->update([
+                    'video_availability' => false
+                ]);
+            else :
+                $video->category->update([
+                    'video_availability' => true
+                ]);
+            endif;
+        elseif ($video->category->parent()->exists()) :
+
+            $videos_count = $video->category->parent->videos->count();
+            foreach ($video->category->parent->children as $cild) :
+                $videos_count = $videos_count + $cild->videos->count();
+            endforeach;
+
+            if ($videos_count == 0) :
+                $video->category->parent->update([
+                    'video_availability' => false
+                ]);
+            else:
+                $video->category->parent->update([
+                    'video_availability' => true
+                ]);
+            endif;
+        endif;
+        if ($video->category->parent()->exists()) :
+            if ($video->category->videos->count() == 0) :
+                $video->category->update([
+                    'video_availability' => false
+                ]);
+            else :
+                $video->category->update([
+                    'video_availability' => true
+                ]);
+            endif;
+        endif;
+
         return redirect()->route('videos.index')->with('status', 'video-updated');
     }
     public function destroy(Video $video)
     {
-        $category = Category::where('id',$video->category_id)->first();
-        $tags = Tag::where('video_id',$video->id)->get();
-        foreach($tags as $tag):
-        $tag->delete();
+        $category = Category::where('id', $video->category_id)->first();
+        $tags = Tag::where('video_id', $video->id)->get();
+        foreach ($tags as $tag) :
+            $tag->delete();
         endforeach;
-        $views = View::where('video_id',$video->id)->get();
-        foreach($views as $view):
-        $view->delete();
+        $views = View::where('video_id', $video->id)->get();
+        foreach ($views as $view) :
+            $view->delete();
         endforeach;
-        $tickets = Ticket::where('video_id',$video->id)->get();
-        foreach($tickets as $ticket):
-        $ticket->delete();
+        $tickets = Ticket::where('video_id', $video->id)->get();
+        foreach ($tickets as $ticket) :
+            $ticket->delete();
         endforeach;
         $video->delete();
-        if (is_null($category)):
-        else:
-            if($category->videos->count()==0):
+        if (is_null($category)) :
+        else :
+            if ($category->videos->count() == 0) :
                 $category->update([
-                    'video_availability'=> false
+                    'video_availability' => false
                 ]);
             endif;
         endif;
         return redirect()->route('videos.index')->with('status', 'video-deleted');
     }
 
-    public function search(Request $request) 
+    public function search(Request $request)
     {
-        if (request('search') == null):
+        if (request('search') == null) :
             $videos = Video::orderBy('id', 'DESC')->paginate(10);
-        else:
-            $videos = Video::where('name', 'ilike', '%' . request('search') . '%')->
-            orWhere('category_id', 'ilike', '%' . (Category::where('name',request('search'))->first()->id ?? request('search')) . '%')->
-            orWhere('event_date', 'like', '%' . request('search') . '%')->paginate(10);
+        else :
+            $videos = Video::where('name', 'ilike', '%' . request('search') . '%')->orWhere('category_id', 'ilike', '%' . (Category::where('name', request('search'))->first()->id ?? request('search')) . '%')->orWhere('event_date', 'like', '%' . request('search') . '%')->paginate(10);
         endif;
 
         return view('videos.index', compact('videos'));
     }
-
 }
