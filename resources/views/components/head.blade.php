c42e7df843380525d944672d7691247ffafdc2c7
Quins <id@quins.ru>
2023-11-04 23:35:28 +0000

    Other fixes

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -28,7 +28,7 @@
     />
 
 
-    <title>Dashboard - Analytics | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+    <title>Spool</title>
 
     <meta name="description" content="" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
